9054f858e6728d3e4ee3d8dd41fd0c44a92a2573
Leonardo Alencar <alencar.leonardo2020@gmail.com>
2021-06-11 13:18:44 -0300

    adding disable button

--- js/script.js
+++ js/script.js
@@ -8,9 +8,11 @@ function iniciar(){
     time = setInterval(()=>{
         timer();
     }, 10)
+    iniciarBtn.setAttribute('disabled', '')
 }
 function parar(){
     clearInterval(time);
+    iniciarBtn.removeAttribute('disabled') 
 }
 function zerar(){
     clearInterval(time);
@@ -19,6 +21,7 @@ function zerar(){
     mm = 0;
     ss = 0;
     ml = 0;
+    iniciarBtn.removeAttribute('disabled')
 }
 
 function timer(){
@@ -38,3 +41,11 @@ function timer(){
     const format = (hh < 10 ? '0' + hh : hh) + " : " + (mm < 10 ? '0' + mm : mm) + " : " + (ss < 10 ? '0' + ss : ss) + " : " + (ml < 10 ? '0' + ml : ml);
     document.querySelector('h2').innerText = format;
 }
+
+const iniciarBtn = document.querySelector('[data-btn="iniciar"]');
+const pararBtn = document.querySelector('[data-btn="parar"]');
+const zerarBtn = document.querySelector('[data-btn="zerar"]');
+
+const iniciou = iniciarBtn.addEventListener('click', iniciar);
+pararBtn.addEventListener('click', parar);
+zerarBtn.addEventListener('click', zerar);
